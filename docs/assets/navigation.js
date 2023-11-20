33629b3403ee45418a729ef80d2796979f5a9c06
kmerten10 <kelsey.merten@gmail.com>
2023-11-20 10:56:40 -0600

    added typedoc files

---
+++ docs/assets/navigation.js
@@ -0,0 +1,1 @@
+window.navigationData = "data:application/octet-stream;base64,H4sIAAAAAAAAE4uuVipJrShRslJyLChwzs8tyM9LzStR0lEqSCzJAIom5yQWF6cW6yPL6mWU5OYAlWRn5qUoWRkaWdTGAgBpON8BRwAAAA=="
--- src/app/user-login-form/user-login-form.component.spec.ts
+++ src/app/user-login-form/user-login-form.component.spec.ts
@@ -1,16 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { UserLoginFormComponent } from './user-login-form.component';
+import { UserLoginComponent } from './user-login-form.component';
 
 describe('UserLoginFormComponent', () => {
-  let component: UserLoginFormComponent;
-  let fixture: ComponentFixture<UserLoginFormComponent>;
+  let component: UserLoginComponent;
+  let fixture: ComponentFixture<UserLoginComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [UserLoginFormComponent]
+      declarations: [UserLoginComponent]
     });
-    fixture = TestBed.createComponent(UserLoginFormComponent);
+    fixture = TestBed.createComponent(UserLoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
